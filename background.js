9c749e45cb722e666da9f1ca1ee0b16887e74ec3
simwai <16225108+simwai@users.noreply.github.com>
2023-07-18 19:06:48 +0200

    style: :art: Used xo linter
    Used xo linter to improve coding style

--- background.js
+++ background.js
@@ -1,90 +1,89 @@
 const websitesData = [
-  { hostSuffix: 'amazon.de', pathPrefix: '/gp/product/' },
-  { hostSuffix: 'amazon.de', pathPrefix: '/dp/' },
-  { hostSuffix: 'de.aliexpress.com' },
+	{hostSuffix: 'amazon.de', pathPrefix: '/gp/product/'},
+	{hostSuffix: 'amazon.de', pathPrefix: '/dp/'},
+	{hostSuffix: 'de.aliexpress.com'},
 ];
 
 chrome.webNavigation.onBeforeNavigate.addListener(handleNavigation, {
-  url: websitesData
+	url: websitesData,
 });
 
 async function handleNavigation(details) {
-  const websites = websitesData.map(x => x.hostSuffix);
-  const currentWebsite = new URL(details.url).hostname;
+	const websites = websitesData.map(x => x.hostSuffix);
+	const currentWebsite = new URL(details.url).hostname;
 
-  if (currentWebsite && websites.includes(currentWebsite)) {
-    console.log('Generating affiliate link URL');
-    const websiteKey = currentWebsite.includes('amazon') ? 'amazon' : currentWebsite.includes('aliexpress') ? 'aliexpress' : '';
-    const affiliateID = await getAffiliateIDForWebsite(websiteKey);
-    if (affiliateID) {
-      const modifiedURL = generateAffiliateLink(details.url, affiliateID, websiteKey);
-      if (modifiedURL !== details.url) {
-        console.log('Modded URL: ', modifiedURL)
-        redirectUser(details.tabId, modifiedURL);
-      }
-    }
-  }
+	if (currentWebsite && websites.includes(currentWebsite)) {
+		console.log('Generating affiliate link URL');
+		const websiteKey = currentWebsite.includes('amazon') ? 'amazon' : (currentWebsite.includes('aliexpress') ? 'aliexpress' : '');
+		const affiliateID = await getAffiliateIDForWebsite(websiteKey);
+		if (affiliateID) {
+			const modifiedURL = generateAffiliateLink(details.url, affiliateID, websiteKey);
+			if (modifiedURL !== details.url) {
+				console.log('Modded URL:', modifiedURL);
+				redirectUser(details.tabId, modifiedURL);
+			}
+		}
+	}
 }
 
 function getAffiliateIDForWebsite(websiteKey) {
-  return new Promise((resolve) => {
-    chrome.storage.sync.get(`${websiteKey}AffiliateId`, (result) => {
-      const affiliateID = result[`${websiteKey}AffiliateId`] || null;
-      resolve(affiliateID);
-    });
-  });
+	return new Promise(resolve => {
+		chrome.storage.sync.get(`${websiteKey}AffiliateId`, result => {
+			const affiliateID = result[`${websiteKey}AffiliateId`] || null;
+			resolve(affiliateID);
+		});
+	});
 }
 
 function generateAffiliateLink(url, affiliateID, websiteKey) {
-  let modifiedURL = removeAffiliateParams(url);
-  
-  if (websiteKey === 'amazon') {
-    modifiedURL = insertAffiliateID(modifiedURL, affiliateID);
-  } else if (websiteKey === 'aliexpress') {
-    modifiedURL = generateAliExpressAffiliateLink(modifiedURL, affiliateID);
-  }
+	let modifiedURL = removeAffiliateParameters(url);
 
-  return modifiedURL;
+	if (websiteKey === 'amazon') {
+		modifiedURL = insertAffiliateID(modifiedURL, affiliateID);
+	} else if (websiteKey === 'aliexpress') {
+		modifiedURL = generateAliExpressAffiliateLink(modifiedURL, affiliateID);
+	}
+
+	return modifiedURL;
 }
 
-function removeAffiliateParams(url) {
-  const baseURL = new URL(url);
-  const params = new URLSearchParams(baseURL.search);
-  
-  // Remove existing affiliate-related parameters
-  params.delete('tag');
-  params.delete('aff_id');
-  
-  baseURL.search = params.toString();
-  return baseURL.href;
+function removeAffiliateParameters(url) {
+	const baseURL = new URL(url);
+	const parameters = new URLSearchParams(baseURL.search);
+
+	// Remove existing affiliate-related parameters
+	parameters.delete('tag');
+	parameters.delete('aff_id');
+
+	baseURL.search = parameters.toString();
+	return baseURL.href;
 }
 
 function insertAffiliateID(url, affiliateID) {
-  const baseURL = new URL(url);
-  const params = new URLSearchParams(baseURL.search);
-  params.set('tag', affiliateID);
-  baseURL.search = params.toString();
-  return baseURL.href;
+	const baseURL = new URL(url);
+	const parameters = new URLSearchParams(baseURL.search);
+	parameters.set('tag', affiliateID);
+	baseURL.search = parameters.toString();
+	return baseURL.href;
 }
 
 function generateAliExpressAffiliateLink(url, affiliateID) {
-  const baseURL = new URL(url);
-  const params = new URLSearchParams(baseURL.search);
-  params.set('aff_id', affiliateID);
-  baseURL.search = params.toString();
-  return baseURL.href;
+	const baseURL = new URL(url);
+	const parameters = new URLSearchParams(baseURL.search);
+	parameters.set('aff_id', affiliateID);
+	baseURL.search = parameters.toString();
+	return baseURL.href;
 }
 
 function redirectUser(tabId, url) {
-  chrome.tabs.update(tabId, { url });
+	chrome.tabs.update(tabId, {url});
 }
-
 
 function getDomainFromCookie(cookie) {
-  const domainParts = cookie.domain.split('.');
-  return domainParts.slice(-2).join('.');
+	const domainParts = cookie.domain.split('.');
+	return domainParts.slice(-2).join('.');
 }
 
-chrome.webNavigation.onBeforeNavigate.addListener((details) => {
-  handleNavigation(details);
-}, { url: websitesData });
+chrome.webNavigation.onBeforeNavigate.addListener(details => {
+	handleNavigation(details);
+}, {url: websitesData});
--- options.js
+++ options.js
@@ -1,58 +1,57 @@
 document.addEventListener('DOMContentLoaded', restoreOptions);
 document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('amazon-affiliate-id').addEventListener('input', handleInputChange);
-  document.getElementById('aliexpress-affiliate-id').addEventListener('input', handleInputChange);
-})
+	document.querySelector('#amazon-affiliate-id').addEventListener('input', handleInputChange);
+	document.querySelector('#aliexpress-affiliate-id').addEventListener('input', handleInputChange);
+});
 
 let amazonAffiliateId = '';
 let aliexpressAffiliateId = '';
 
 function saveOptions() {
-  chrome.storage.sync.set(
-    {
-      amazonAffiliateId: amazonAffiliateId,
-      aliexpressAffiliateId: aliexpressAffiliateId,
-    },
-    () => {
-      const status = document.getElementById('status');
-      status.textContent = 'Options saved.';
-      setTimeout(() => {
-        status.textContent = '';
-      }, 1500);
-    }
-  );
+	chrome.storage.sync.set(
+		{
+			amazonAffiliateId,
+			aliexpressAffiliateId,
+		},
+		() => {
+			const status = document.querySelector('#status');
+			status.textContent = 'Options saved.';
+			setTimeout(() => {
+				status.textContent = '';
+			}, 1500);
+		},
+	);
 }
 
 function restoreOptions() {
-  chrome.storage.sync.get(
-    {
-      amazonAffiliateId: '',
-      aliexpressAffiliateId: '',
-    },
-    (items) => {
-      const amazonAffiliateIdInput = document.getElementById('amazon-affiliate-id');
-      const aliexpressAffiliateIdInput = document.getElementById('aliexpress-affiliate-id');
+	chrome.storage.sync.get(
+		{
+			amazonAffiliateId: '',
+			aliexpressAffiliateId: '',
+		},
+		items => {
+			const amazonAffiliateIdInput = document.querySelector('#amazon-affiliate-id');
+			const aliexpressAffiliateIdInput = document.querySelector('#aliexpress-affiliate-id');
 
-      amazonAffiliateIdInput.setAttribute("placeholder", items.amazonAffiliateId?.length > 0 ? items.amazonAffiliateId : 'Enter your Amazon.de affiliate ID');
-      aliexpressAffiliateIdInput.setAttribute("placeholder", items.aliexpressAffiliateId?.length > 0 ? items.aliexpressAffiliateId : 'Enter your AliExpress.com affiliate ID')
+			amazonAffiliateIdInput.setAttribute('placeholder', items.amazonAffiliateId?.length > 0 ? items.amazonAffiliateId : 'Enter your Amazon.de affiliate ID');
+			aliexpressAffiliateIdInput.setAttribute('placeholder', items.aliexpressAffiliateId?.length > 0 ? items.aliexpressAffiliateId : 'Enter your AliExpress.com affiliate ID');
 
-      amazonAffiliateId = items.amazonAffiliateId;
-      aliexpressAffiliateId = items.aliexpressAffiliateId;
-    }
-  );
+			amazonAffiliateId = items.amazonAffiliateId;
+			aliexpressAffiliateId = items.aliexpressAffiliateId;
+		},
+	);
 }
 
-
 function handleInputChange(event) {
-  const target = event.target;
-  const value = target.value;
-  console.log(event)
+	const target = event.target;
+	const value = target.value;
+	console.log(event);
 
-  if (target.id === 'amazon-affiliate-id') {
-    amazonAffiliateId = value;
-  } else if (target.id === 'aliexpress-affiliate-id') {
-    aliexpressAffiliateId = value;
-  }
+	if (target.id === 'amazon-affiliate-id') {
+		amazonAffiliateId = value;
+	} else if (target.id === 'aliexpress-affiliate-id') {
+		aliexpressAffiliateId = value;
+	}
 
-  saveOptions();
+	saveOptions();
 }
