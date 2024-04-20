82b4f0a0401e626d547b6d1d61acb600dfe66bf5
Simon Waiblinger <16225108+simwai@users.noreply.github.com>
2024-04-20 20:31:51 +0200

    Delete dist directory

--- dist/options.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function saveOptions(): Promise<void>;
-export declare function restoreOptions(): Promise<void>;
-export declare function handleInputChange(event: Event): Promise<void>;
--- dist/tests/test-types.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=test-types.js.map
